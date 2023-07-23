55a8fd4cb385b3c074aa544a79c67d3f379b966d
MunsMan <munsman.github@gmail.com>
2023-07-23 12:38:56 +0200

    [nvim] adding wrapping extention for text files like latex

--- nvim/lua/plugins/wrapping.lua
+++ nvim/lua/plugins/wrapping.lua
@@ -1,4 +1,18 @@
 return {
-    "andrewferrier/wrapping.nvim"
+    "andrewferrier/wrapping.nvim",
+    config = function()
+        require("wrapping").setup()
+    end,
+    opts = {
+        auto_set_mode_filetype_allowlist = {
+            "asciidoc",
+            "gitcommit",
+            "latex",
+            "mail",
+            "markdown",
+            "rst",
+            "tex",
+            "text",
+        },
+    }
 }
-
