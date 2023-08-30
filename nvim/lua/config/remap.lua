ddaf175f30691a0d5853f4726551fbdef1770c54
MunsMan <munsman.github@gmail.com>
2023-08-30 15:46:39 +0200

    [nvim] adding lazy event and moving shortcuts

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -54,14 +54,4 @@ vim.keymap.set({ "n", "v" }, "gh", "^", { remap = true, desc = 'Move to first ch
 -- vim.keymap.set("i", "<Esc>o", "ö")
 
 
--- Trouble Remaps
-vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end, { desc = "trouble" })
-vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end,
-    { desc = "workspace diagnostics" })
-vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end,
-    { desc = "document diagnostics" })
-vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end, { desc = "quickfix list" })
-vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end, { desc = "local list" })
-vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end, { desc = "lsp references" })
-
 vim.g.tmux_navigator_no_mappings = 1
