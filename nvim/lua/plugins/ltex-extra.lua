a94e3567b0cb4a217a74c489739dd7488be2f64f
MunsMan <munsman.github@gmail.com>
2023-08-30 15:43:11 +0200

    [nvim] adding ltex with add to dict support

---
+++ nvim/lua/plugins/ltex-extra.lua
@@ -0,0 +1,19 @@
+return {
+    "barreiroleo/ltex_extra.nvim",
+    lazy = true,
+    ft = { "markdown", "tex" },
+    dependencies = { "neovim/nvim-lspconfig" },
+    -- yes, you can use the opts field, just I'm showing the setup explicitly
+    config = function()
+        require("ltex_extra").setup {
+            server_opts = {
+                on_attach = function(client, bufnr)
+                    -- your on_attach process
+                end,
+                settings = {
+                    ltex = {}
+                }
+            },
+        }
+    end
+}
