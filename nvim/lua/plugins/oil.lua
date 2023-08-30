15ec47ba695e36327fd989dd78f2c0d57067e493
MunsMan <munsman.github@gmail.com>
2023-08-30 15:43:56 +0200

    [nvim] adding lazy and some options and moving shortcut

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,8 @@
 return {
     'stevearc/oil.nvim',
+    lazy = false,
     opts = {
+        default_file_explorer = true,
         skip_confirm_for_simple_edits = true,
         view_options = {
             show_hidden = true
@@ -9,9 +11,16 @@ return {
             padding = 10,
             win_options = {
                 winblend = 5
-            }
+            },
+            border = "rounded"
+        },
+        preview = {
+            width = 0.4
         }
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+        { "<leader>e", function() require('oil').toggle_float() end, desc = 'Open Oil', mode = { 'n', 'v' } }
+    }
 }
