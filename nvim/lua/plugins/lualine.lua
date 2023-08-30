8fd4557501ac564ee1f7fdf2679ac30946795c00
MunsMan <munsman.github@gmail.com>
2023-08-30 18:24:26 +0200

    [nvim] updating lazy config for update in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,26 @@
 return {
     'nvim-lualine/lualine.nvim',
-    opts = {
-        theme = 'onedark'
-    }
+    event = 'VeryLazy',
+    opts = function()
+        local lazy_status = require("lazy.status")
+        return {
+            options = {
+                theme = 'onedark'
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates
+                    }, { 'encoding' }, { 'fileformat' }, { 'filetype' },
+                }
+                ,
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+        }
+    end
 }
-
