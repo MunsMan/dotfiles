4629e54099f49601d2e94ad1d3ca9b86c39668a0
MunsMan <munsman.github@gmail.com>
2023-08-30 15:38:52 +0200

    [nvim] updating flash keyboard shortcuts

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,12 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {
-        modes = { char = { enabled = false } }
+        modes = {
+            char = {
+                keys =
+                { ['f'] = 't', ['F'] = 'T', ['t'] = '<leader>t', ['T'] = '<leader>T' }
+            }
+        }
     },
     keys = {
         {
