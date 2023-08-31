f21a33acfa2197d29a08410f0a64695da42f1f77
MunsMan <munsman.github@gmail.com>
2023-08-31 15:06:26 +0200

    [nvim] fixing auto detection in ltex_extra

--- nvim/lua/plugins/ltex-extra.lua
+++ nvim/lua/plugins/ltex-extra.lua
@@ -1,19 +1,26 @@
 return {
     "barreiroleo/ltex_extra.nvim",
-    lazy = true,
     ft = { "markdown", "tex" },
     dependencies = { "neovim/nvim-lspconfig" },
     -- yes, you can use the opts field, just I'm showing the setup explicitly
     config = function()
-        require("ltex_extra").setup {
-            server_opts = {
-                on_attach = function(client, bufnr)
-                    -- your on_attach process
-                end,
-                settings = {
-                    ltex = {}
+        require("lspconfig").ltex.setup {
+            on_attach = function(client, bufnr)
+                require("ltex_extra").setup {
+                    load_langs = { "en-US" },
+                    init_check = true,
+                    path = ".ltex",
+                    log_level = "none",
                 }
-            },
+            end,
+            settings = {
+                ltex = {
+                    language = "en-US",
+                    additionalRules = {
+                        enablePickyRules = true,
+                    }
+                }
+            }
         }
     end
 }
