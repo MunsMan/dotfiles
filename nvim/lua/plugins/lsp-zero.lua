56bbc1c0332259f6d62024fca871cd0444522f5b
MunsMan <munsman.github@gmail.com>
2023-09-16 10:46:29 +0200

    [nvim] upgrading lua setup

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -61,12 +61,39 @@ return {
 
             -- example to setup lua_ls and enable call snippets
             lspconfig.lua_ls.setup({
+                commands = {
+                    Format = {
+                        function()
+                            require("stylua-nvim").format_file()
+                        end,
+                    },
+                },
                 settings = {
                     Lua = {
                         completion = {
                             callSnippet = "Replace"
-                        }
-                    }
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { "vim" },
+                        },
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                    format = {
+                        enable = true,
+                        defaultConfig = {
+                            indent_style = "space",
+                            indent_size = "2",
+                        },
+                    },
                 }
             })
             lsp.ensure_installed({ 'tsserver', 'eslint', 'lua_ls', 'rust_analyzer', 'ltex' })
---
+++ nvim/lua/plugins/stylua.lua
@@ -0,0 +1,6 @@
+return {
+    "ckipp01/stylua-nvim",
+    lazy = true,
+    ft = "lua",
+    build = "cargo install stylua",
+}
