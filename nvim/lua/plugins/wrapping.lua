7fae936b02418ab25d11c6b471c37d0275a2b10c
MunsMan <munsman.github@gmail.com>
2023-10-18 21:01:20 +0200

    [nvim] allways wrap on markdown

--- nvim/lua/plugins/wrapping.lua
+++ nvim/lua/plugins/wrapping.lua
@@ -1,33 +1,35 @@
 return {
-    'andrewferrier/wrapping.nvim',
-    ft = { 'markdown', 'tex' },
+    "andrewferrier/wrapping.nvim",
+    ft = { "markdown", "tex" },
     opts = {
         auto_set_mode_filetype_allowlist = {
-            'asciidoc',
-            'gitcommit',
-            'latex',
-            'mail',
-            'markdown',
-            'rst',
-            'tex',
-            'text',
+            "asciidoc",
+            "gitcommit",
+            "latex",
+            "mail",
+            "markdown",
+            "rst",
+            "tex",
+            "text",
         },
-        softener = { markdown = 1.6, tex = true },
+        softener = { markdown = true, tex = true },
         notify_on_switch = false,
-        create_keymaps = false
+        create_keymaps = false,
     },
     config = function(_, opts)
-        require('wrapping').setup(opts)
-        vim.keymap.set({ 'n', 'v' }, 'n', 'gj')
-        vim.keymap.set({ 'n', 'v' }, 'e', 'gk')
-        vim.keymap.set({ 'n', 'v' }, 'gi', 'g$')
-        vim.keymap.set({ 'n', 'v' }, 'gh', 'g0')
+        require("wrapping").setup(opts)
+        vim.keymap.set({ "n", "v" }, "n", "gj")
+        vim.keymap.set({ "n", "v" }, "e", "gk")
+        vim.keymap.set({ "n", "v" }, "gi", "g$")
+        vim.keymap.set({ "n", "v" }, "gh", "g0")
     end,
     keys = {
         {
-            '<leader>cw',
-            function() require('wrapping').toggle_wrap_mode() end,
-            desc = 'Toggle Wrap Mode'
-        }
-    }
+            "<leader>cw",
+            function()
+                require("wrapping").toggle_wrap_mode()
+            end,
+            desc = "Toggle Wrap Mode",
+        },
+    },
 }
