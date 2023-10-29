3906664ca58ec42d39b8d2548f1739d702403246
MunsMan <munsman.github@gmail.com>
2023-10-29 16:08:58 +0100

    [nix] trying to get home manager to work

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,24 +2,24 @@
   description = "Home Manager Flake";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/release-23.05"; 
+      nixpkgs.url = "github:NixOS/nixpkgs/release-23.11"; 
       home-manager = { 
           url = "github:nix-community/home-manager";
           inputs.nixpkgs.follows = "nixpkgs";
      };
   };
 
-  outputs = inputs: {
+  outputs = { home-manager, nixpkgs, ... }: 
+    let
+        system = "x86_64-darwin";
+    in {
       homeConfigurations = {
-        mbp = inputs.home-manager.lib.homeManagerConfiguration {
-            system = "x86_64-darwin";
-            homeDirectory = "/Users/munsman";
-            username = "munsman";
-            configuration.imports = [
-                "./modules/home-manager.nix"
+        mbp = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+                ./modules/home-manager.nix
             ];
-
-            };
+      };
     };
-  };
+};
 }
--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -1,4 +1,15 @@
-{ config, pkgs, libs }:
+{ config, lib, pkgs, ... }:
+let
+ username = "munsman";
+ in
 {
+    home = {
+
+                        inherit username;
+                        homeDirectory = "/Users/${username}";
+                        stateVersion = "23.11";
+
+        };
+
     programs.home-manager.enable = true;
 }
