e888e86449a4c7c316b8a361dbb2854f34775a98
MunsMan <munsman.github@gmail.com>
2024-02-01 20:47:32 +0100

    [neovim] allowing copilot in markdown

--- darwin/modules/home-manager/editors/neovim/lua/plugins/copilot.lua
+++ darwin/modules/home-manager/editors/neovim/lua/plugins/copilot.lua
@@ -4,10 +4,16 @@ return {
     event = "InsertEnter",
     opts = {
         suggestion = {
+            enabled = true,
             auto_trigger = true,
             keymap = {
                 accept = "<C-u>",
             },
+        },
+        panel = { enabled = true },
+        filetypes = {
+            markdown = true,
+            yaml = true,
         },
     },
     keys = {
